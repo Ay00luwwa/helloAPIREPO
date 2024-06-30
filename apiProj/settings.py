dd8f3442ce6f010d2a1e681801a4b93f51826c7a
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-07-01 00:52:02 +0100

    commit 4

--- apiProj/settings.py
+++ apiProj/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,13 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = "django-insecure-33_9@)^*@metz)a9q+2z(a4!+a+5=w9-=7!n@+ay734b)kr8%@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*", "localhost", "http://127.0.0.1:"]
+ALLOWED_HOSTS = ["*", "localhost", "http://127.0.0.1:", "https://ayooluwa.pythonanywhere.com/"]
 
 
 # Application definition
